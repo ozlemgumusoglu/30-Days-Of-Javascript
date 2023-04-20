461b53f32fb47014105360c708a699642fd1ed12
Ozlem Gumusoglu <ozlemgumusoglu@gmail.com>
2023-04-20 18:55:45 +0300

    first my commit

---
+++ Day1-js/index.js
@@ -0,0 +1,11 @@
+// ******************DAY-1-EXERSIZE********************
+
+
+// 1.comments can make code readable...
+// 2.Welcome to 30DaysOfJavaScript
+
+/* 
+3.comments can make code readable,
+easyto reuse and informative.
+*/
+
---
+++ Day1-js/variable.js
@@ -0,0 +1,46 @@
+let string = "kelime";
+let boolean = true;
+let undefined
+let bos = null;
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log(typeof string)
+
+
+// declare 4 variables without assigning a values
+
+let what ;
+let happened;
+let see;
+let how;
+
+// declare 4 variables assigned a values
+
+let ne = "what";
+let olay = true;
+let sayi = 56;
+let nasil = "how";
+
+// // Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let name = "ozlem"
+let lastname= "gumusoglu"
+let status= "unmarried"
+let country = "turkey "
+let age = 32 
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let isim = "ozlem gumusoglu" ,
+    yas =32,
+    durum="bekar",
+    ulke="turkiye"
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+
+let myAge = 32;
+let yourAge = 36
+
+console.log(`i am ${myAge} years old.\n you are ${yourAge} years old.`)
